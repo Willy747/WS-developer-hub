2c4561c5bb7a1b497e1ea57cf7ce5cb9c8be3434
Will Stayte <wrstayte@gmail.com>
2026-06-23 14:09:02 -0400

    Added skills, resume, learning roadmap, and homepage redesign

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
+if (localStorage.getItem("theme") === "dark") {
+
+    document.body.classList.add("dark-mode");
+
+}
+
+
 fetch("sidebar.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("sidebar-container").innerHTML = data;
     });
-    
+
+    document.addEventListener("click", function(event) {
+
+    if (event.target.id === "darkModeBtn") {
+
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+
+            localStorage.setItem("theme", "dark");
+
+        } else {
+
+            localStorage.setItem("theme", "light");
+
+        }
+
+    }
+
+});
+
